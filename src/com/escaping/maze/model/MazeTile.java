511a3e3643f17c658504e5276cc6ddb67f2ecc46
Ece <akinecee3535@hotmail.com>
2025-04-25 22:10:23 +0300

    MazeTile ve structures eklendi

--- src/com/escaping/maze/model/MazeTile.java
+++ src/com/escaping/maze/model/MazeTile.java
@@ -1,5 +1,35 @@
 package com.escaping.maze.model;
 
+
 public class MazeTile {
-//ecenin
+    private int x, y; // coordinates
+    private char type; // 'E', 'W', 'T', 'P', 'G'
+    private boolean hasAgent;
+
+    public MazeTile(int x, int y, char type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.hasAgent = false;
+    }
+
+    public boolean isTraversable() {
+        return type != 'W'; // wall değilse geçebilir
+    }
+
+    public String toString() {
+        if (hasAgent) return "A";
+        return String.valueOf(type);
+    }
+
+    // Getter ve Setter'lar
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public char getType() { return type; }
+    public boolean hasAgent() { return hasAgent; }
+
+    public void setType(char type) { this.type = type; }
+    public void setHasAgent(boolean hasAgent) { this.hasAgent = hasAgent; }
 }
+
+
